384d55df4542115f707ead20b506bec210399505
nagarajduraisamy <nagaraj.duraisamy@socgen.com>
2019-10-15 09:01:17 +0530

    Login page and Registration page added

--- Client/src/app/login/login.component.ts
+++ Client/src/app/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 
-import { products } from '../products';
 
 @Component({
   selector: 'app-login',
@@ -8,12 +7,7 @@ import { products } from '../products';
   styleUrls: ['./login.component.css']
 })
 export class ProductListComponent {
-  products = products;
-
-  share() {
-    window.alert('The product has been shared!');
-  }
-}
+ }
 
 
 /*
